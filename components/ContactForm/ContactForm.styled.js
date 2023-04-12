4567d971b8aeb345600bbcb43244f865162e6dfc
Igor Smelyanets <ismelyanets@yahoo.com>
2023-04-12 18:00:27 +0300

    Updates

---
+++ components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,10 @@
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+import styled from '@emotion/styled';
+export var Form = styled.form(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  outline: 1px solid black;\n  width: 350px;\n  display: flex;\n  flex-direction: column;\n  padding: 15px;\n"], ["\n  outline: 1px solid black;\n  width: 350px;\n  display: flex;\n  flex-direction: column;\n  padding: 15px;\n"])));
+export var Label = styled.label(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  margin-bottom: 5px;\n  padding: 5px;\n  font-weight: 900;\n"], ["\n  margin-bottom: 5px;\n  padding: 5px;\n  font-weight: 900;\n"])));
+export var Input = styled.input(templateObject_3 || (templateObject_3 = __makeTemplateObject(["\n  margin-bottom: 10px;\n  border-radius: 5px;\n"], ["\n  margin-bottom: 10px;\n  border-radius: 5px;\n"])));
+export var Button = styled.button(templateObject_4 || (templateObject_4 = __makeTemplateObject(["\n  width: 150px;\n  margin: 5px auto;\n  background-color: #2196f3;\n  border: none;\n  border-radius: 2px;\n  color: white;\n  cursor: pointer;\n"], ["\n  width: 150px;\n  margin: 5px auto;\n  background-color: #2196f3;\n  border: none;\n  border-radius: 2px;\n  color: white;\n  cursor: pointer;\n"])));
+var templateObject_1, templateObject_2, templateObject_3, templateObject_4;
---
+++ components/ContactList/ContactList.styled.js
@@ -0,0 +1,7 @@
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
+import styled from '@emotion/styled';
+export var Contacts = styled.ul(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  padding-left: 0;\n"], ["\n  padding-left: 0;\n"])));
+var templateObject_1;
